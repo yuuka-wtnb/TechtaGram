d078aae2f4645cc783b6292f9d338eed1a5e20f9
Yuka Watanabe <yuuka.wtnb.011@gmail.com>
2020-09-16 01:53:56 +0900

    Initial Commit

---
+++ TechtaGram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechtaGram
+//
+//  Created by Yuuka Watanabe on 2020/09/16.
+//  Copyright © 2020 Yuka Watanabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
